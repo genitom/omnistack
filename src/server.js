b2eb36f56902b00fcc168ddfd1aa705e2e04009c
Geniton lima <genitom.wd@gmail.com>
2019-04-12 15:09:15 -0300

    atualizacao

--- src/server.js
+++ src/server.js
@@ -18,8 +18,13 @@ mongoose.connect('mongodb+srv://genitom:69588878@cluster0-gx7qk.mongodb.net/omni
 // app.get('/teste', (req, res) => { // todos os (req, res) é um middleware, middleware é um interceptador.
 //   return res.send('Hello word!!!')
 // })
+app.use((req,res,next) => {
+  req.io = io;
+  return next();
+});
 app.use(express.json())  // app.use é para cadastrar um modulo dentro do express, express.json() ajuda a entender as req em formato json
 app.use(express.urlencoded({ extended: true })); // Ele permite que agente envie arquivos em nossas req.
 app.use('/files', express.static(path.resolve(__dirname, "..", "tmp")));
 app.use(require('./routes'))
+
 app.listen(process.env.PORT || 3339)
