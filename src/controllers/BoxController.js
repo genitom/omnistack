439deaec5579fa1e9d16462c0f1203e448e419f9
Geniton lima <genitom.wd@gmail.com>
2019-04-12 15:52:27 -0300

    sdsd

--- src/controllers/BoxController.js
+++ src/controllers/BoxController.js
@@ -2,7 +2,7 @@ const Box = require('../models/box')
 
 class BoxController {
   async store (req, res) {
-    const box = await Box.create({ title: 'Rocketseat' });
+    const box = await Box.create({ title: '' });
 
     return res.json(box)
   }
