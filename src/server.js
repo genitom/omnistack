937707a303d007798dc70130215fc294fe6c81bb
Geniton lima <genitom.wd@gmail.com>
2019-04-12 14:38:11 -0300

    backend

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const cors = require('cors');
+
+io.on("connection", socket => {
+  socket.on('connectRoom', box => {
+    socket.join(box)
+  })
+})
+
+mongoose.connect('mongodb+srv://genitom:69588878@cluster0-gx7qk.mongodb.net/omnistack?retryWrites=true',{
+  useNewUrlParser: true
+})
+// app.get('/teste', (req, res) => { // todos os (req, res) é um middleware, middleware é um interceptador.
+//   return res.send('Hello word!!!')
+// })
+app.use(express.json())  // app.use é para cadastrar um modulo dentro do express, express.json() ajuda a entender as req em formato json
+app.use(express.urlencoded({ extended: true })); // Ele permite que agente envie arquivos em nossas req.
+app.use('/files', express.static(path.resolve(__dirname, "..", "tmp")));
+app.use(require('./routes'))
+app.listen(3339)
