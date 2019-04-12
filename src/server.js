e8eb633a897bc68b8c91a9ca9fcf95aae98a9e2e
Geniton lima <genitom.wd@gmail.com>
2019-04-12 15:22:24 -0300

    dsd

--- src/server.js
+++ src/server.js
@@ -8,9 +8,6 @@ const cors = require('cors');
 
 app.use(cors());
 
-const server = require('http').Server(app);
-const io = require("socket.io")(server);
-
 io.on("connection", socket => {
   socket.on('connectRoom', box => {
     socket.join(box)
